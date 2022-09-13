d0ec2258c1429eacca1cdd58268af51c76bd0244
antonio195 <antonio195br@gmail.com>
2022-09-13 15:38:00 -0300

    Telas de login, registro e recuperação de senha prontas com as devidas validações.

---
+++ OnlyBooks/app/src/main/java/com/antoniocostadossantos/onlybooks/ForgotPasswordActivity.kt
@@ -0,0 +1,67 @@
+package com.antoniocostadossantos.onlybooks
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.antoniocostadossantos.onlybooks.databinding.ActivityForgotPasswordBinding
+import com.antoniocostadossantos.onlybooks.databinding.ActivityLoginBinding
+
+class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityForgotPasswordBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.sendEmail.setOnClickListener {
+            checkFields()
+        }
+
+        binding.loginButton.setOnClickListener {
+            loginActivity()
+        }
+
+
+    }
+
+    private fun loginActivity() {
+        val intent = Intent(this@ForgotPasswordActivity, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun checkFields() {
+        when {
+            binding.emailInput.text.toString().isEmpty() -> {
+                binding.emailInput.error = "Digite o email"
+            }
+
+            !emailValidation(binding.emailInput.text.toString()) -> {
+                binding.emailInput.error = "Digite um email válido"
+            }
+            else -> {
+                getNewPassword()
+            }
+        }
+    }
+
+    private fun emailValidation(email: String): Boolean {
+        val pattern =
+            Regex(
+                "^([a-zA-Z0-9_\\-.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|" +
+                        "(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})\$"
+            )
+        val result = pattern.containsMatchIn(email)
+        return result
+    }
+
+    private fun getNewPassword() {
+        Toast.makeText(this, "Gerando nova senha...", Toast.LENGTH_SHORT).show()
+    }
+}
